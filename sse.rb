2582b7544147fdd73148ede5fed6dc87cf0e487f
Joe Zhou@mac <joezhou@msn.cn>
2013-03-07 00:40:57 +0800

    added pubsub

--- sse.rb
+++ sse.rb
@@ -1,14 +1,57 @@
+$: << File.dirname(__FILE__)
 require 'goliath'
+require 'pubsub'
+#require 'awesome_print'
+
  
 class SSE < Goliath::API
-  use Rack::Static, :urls => ["/index.html"], :root => Goliath::Application.app_path("public")
- 
+
+#  use Rack::Static, :urls => ["/index.html"], :root => Goliath::Application.app_path("public")
+
+@@myenv = nil
+
+  # handles all connection close events
+  def on_close(env)
+    Pubsub.callbacks(:on_close) { |callback| callback.call(env) }
+  end
+
   def response(env)
-    EM.add_periodic_timer(1) { env.stream_send("data:hello ##{rand(100)}\n\n") }
-    EM.add_periodic_timer(3) do
-      env.stream_send(["event:signup", "data:signup event ##{rand(100)}\n\n"].join("\n"))
+
+    logger.info "env request path = #{env['REQUEST_PATH']}"
+    if env['REQUEST_PATH'] == '/'
+      @@myenv = env
+      [200, {'Content-Type' => 'text/html'}, File.open('public/index.html').read()]
+#    elsif env['REQUEST_PATH'] == '/events'
+#      logger.info "start response"
+#      streaming_response(200, {'Content-Type' => 'text/event-stream'})
+    elsif env['REQUEST_PATH'] == '/0'
+      @@myenv.stream_send("data:hello ##{rand(100)}\n\n") 
+    elsif env['REQUEST_PATH'] == '/1'
+      @@myenv.stream_send(["event:signup", "data:signup event ##{rand(100)}\n\n"].join("\n"))
+    elsif env['REQUEST_PATH'] == '/events'
+      unless env['HTTP_ACCEPT'] == 'text/event-stream'
+        return [ 406, { }, [ ] ]
+      end
+        
+      sub_id = Pubsub.channel.subscribe do |msg|
+        env.stream_send("data:hello #{msg}\n\n")
+      end
+    
+      env['pubsub.subscriber.id'] = sub_id
+    
+      Pubsub.callback(:on_close, sub_id) do |e|
+        if e['pubsub.subscriber.id'] == sub_id
+          Pubsub.channel.unsubscribe(sub_id)
+          Pubsub.remove(:on_close, sub_id)
+        end
+      end
+    
+      streaming_response(200, { 'Content-Type' => "text/event-stream" })
+    elsif env['REQUEST_PATH'] == '/message'
+      Pubsub.channel.push("data:broadcasting message..\n\n")
+      [ 200, { }, [ ] ]
     end
- 
-    streaming_response(200, {'Content-Type' => 'text/event-stream'})
   end
 end
+
+
